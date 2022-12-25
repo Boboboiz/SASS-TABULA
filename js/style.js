2d2d4b4a9372b0b24604e4d16b40bbfebb768573
Huy <lenambaohuy107@gmail.com>
2022-12-25 15:29:10 +0700

    work load

--- js/style.js
+++ js/style.js
@@ -2,3 +2,8 @@ window.addEventListener("scroll", function (){
     let stickNav = document.querySelector("header")
     stickNav.classList.toggle('sticky', window.scrollY > 0)
 })
+
+let toggle = document.querySelector('.toggle')
+toggle.addEventListener('click', function(){
+    
+})
